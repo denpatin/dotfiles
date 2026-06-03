32dd1ade6ff2ed0b3a95d5258938863439b58071
Den Patin <hi@dpat.in>
2026-06-03 15:45:18 +0800

    Update config.fish

--- config.fish
+++ config.fish
@@ -119,6 +119,4 @@ function vois
     git fetch origin --quiet
 end
 
-# Added by OrbStack: command-line tools and integration
-# This won't be added again if you remove it.
 source ~/.orbstack/shell/init2.fish 2>/dev/null || :
